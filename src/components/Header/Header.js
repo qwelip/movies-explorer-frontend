608daf92a7f2037d0a8ad09d7f16ceede01f7527
AndMed <andmed20@yandex.ru>
2022-06-26 15:28:46 +0300

    верстка страницы с фильмами + адаптив

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../images/logo.png'
+import menu from '../../images/menu.png';
 import avatar from '../../images/ava.png'
 import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
-const regAndLoginBlock = () => {
+const RegAndLoginBlock = () => {
   return (
     <nav className='header__nav'>
       <a className='header__reg' href="">
@@ -19,14 +20,32 @@ const regAndLoginBlock = () => {
   )
 }
 
-const accountBlock = () => {
+const AccountBlock = () => {
   return (
-    <div className="header__account">
-      <p className='header__account-name'>Account</p>
+    <div className="header__account-block">
+      <p className='header__account-block-name'>Account</p>
       <img className='header__avatar' src={avatar} alt="Аватар" />
     </div>
   )
 }
+
+const PopupMenu = ({children}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+  return (
+    <div className='header__popup-menu'>
+      <button onClick={handleClick} className='header__popup-menu-btn'>
+        <img src={menu} alt="Меню" />
+      </button>
+
+      <nav className={isOpen ? "header__popup-menu-block" : "header__popup-menu-block header__popup-menu-block_closed"}>
+        {children.map(item => item)}
+      </nav>
+    </div>
+  );
+};
 
 const Header = () => {
   return (
@@ -34,9 +53,17 @@ const Header = () => {
       <div className="wrapper">
         <div className="header__block">
           <img className='header__logo' src={logo} alt="Логотип" />
-          <Navigation/>
-          {/* {regAndLoginBlock()} */}
-          {accountBlock()}
+          <div className="header__navigation-hider">
+            <Navigation/>
+          </div>
+          <div className="header__account-block-hider">
+            <AccountBlock/>
+          </div>
+          {/* <RegAndLoginBlock/> */}
+          <PopupMenu>
+            <Navigation/>
+            <AccountBlock/>
+          </PopupMenu>
         </div>
       </div>
     </header>
